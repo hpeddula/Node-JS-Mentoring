7ae3d140f32de860756f6cba21b94fe144b92db1
Harsha Peddula <harsha_peddula@epam.com>
2021-03-15 13:35:30 +0530

    Adding task 3

--- Modules/Module1/Task2/Task2.js
+++ Modules/Module1/Task2/Task2.js
@@ -1,7 +1,53 @@
-const csvFilePath='../nodejs-hw1-ex1.csv';
-const csv=require('csvtojson')
-csv()
-.fromFile('C:\\Users\\Harsha_Peddula\\Desktop\\Node JS Mentoring\\Modules\\Module1\\nodejs-hw1-ex1.csv')
-.then((jsonObj)=>{
-    console.log(jsonObj);
-}).catch(err => console.log(err))
+
+const csv = require('csvtojson');
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+console.log(path.join(__dirname, '../nodejs-hw1-ex1.csv'));
+const csvFilePath = path.join(__dirname, '../nodejs-hw1-ex1.csv');
+const os = require('os');
+
+const convertToJSON = () => {
+    return new Promise((resolve, reject) => {
+        csv({ignoreEmpty:true,downstreamFormat:'line'})
+            .fromFile(csvFilePath)
+            .then((jsonObj) => {
+                fs.writeFile(path.join(__dirname, '../Output.json'), JSON.stringify(jsonObj), (err) => {
+                    if (err) {
+                        reject(403)
+                    }
+                    resolve(200)
+                })
+            })
+            .catch(() => {
+                reject(403)
+            })
+    })
+}
+
+const writeToTextFile = async () => {
+    const response = await convertToJSON();
+    if (response === 200) {
+        const fileStream = fs.createReadStream(path.join(__dirname, '../Output.json'));
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+
+        for await (const line of rl) {
+            const data = JSON.parse(line);
+            for(d of data) {
+                console.log(JSON.stringify(d))
+            fs.appendFile(path.join(__dirname, '../nodejs-hw1-ex2.txt'), JSON.stringify(d) + os.EOL, (err) => {
+                if (err) {
+                    console.log(err)
+                }
+            })
+        }
+        }
+    }
+}
+const main = async () => {
+    await writeToTextFile();
+}
+main();
---
+++ Modules/Module1/Task3/Task3.js
@@ -0,0 +1,63 @@
+
+import csv from 'csvtojson';
+import { createInterface } from 'readline';
+import { writeFile, createReadStream, appendFile } from 'fs';
+import { join } from 'path';
+import { EOL } from 'os';
+
+
+
+const csvPath = '../Module1/nodejs-hw1-ex1.csv';
+const jsonFile = './Output.json';
+const txtFile = './nodejs-hw1-ex2.txt';
+const csvFilePath = join(__dirname,csvPath);
+
+
+const convertToJSON = () => {
+    return new Promise((resolve, reject) => {
+        csv({ ignoreEmpty: true, downstreamFormat: 'line' })
+            .fromFile(csvFilePath)
+            .then((jsonObj) => {
+                writeFile(join(__dirname,jsonFile), JSON.stringify(jsonObj), (err) => {
+                    if (err) {
+                        reject(403)
+                    }
+                    resolve(200)
+                })
+            })
+            .catch(() => {
+                reject(403)
+            })
+    })
+}
+
+const writeToTextFile = async () => {
+    try {
+        const response = await convertToJSON();
+        if (response === 200) {
+            const fileStream = createReadStream(join(__dirname,jsonFile));
+            const rl = createInterface({
+                input: fileStream,
+                crlfDelay: Infinity
+            });
+
+            for await (const line of rl) {
+                const data = JSON.parse(line);
+                for (let d of data) {
+                    console.log(JSON.stringify(d))
+                    appendFile(join(__dirname,txtFile), JSON.stringify(d) + EOL, (err) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                    })
+                }
+            }
+        }
+    } catch (e) {
+        console.log('Error', e)
+    }
+}
+const main = async () => {
+    await writeToTextFile();
+}
+main();

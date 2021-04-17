84ae2b90402f1f0732d0ebfe23109d0adf21c97c
Harsha Peddula <harsha_peddula@epam.com>
2021-04-17 17:15:35 +0530

    First commit module 4

---
+++ Modules/Module4/server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config();
+
+
+
+const user = require('./controllers/UserRoutes');
+const group = require('./controllers/GroupRoutes');
+
+const app = express();
+
+
+
+// parse application/json
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
+
+app.use('/user',user)
+app.use('/group',group)
+
+// default URL to API
+app.use('/', (req, res) => {
+    res.status(200).send('Server Works');
+});
+
+const server = http.createServer(app);
+const port = process.env.SERVER_PORT || 3001;
+server.listen(port);
+
+console.log('Port',process.env.SERVER_PORT)
+
+console.debug(`Server listening on port ${port}`  );
+
+
+
